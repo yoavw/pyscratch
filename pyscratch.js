f24860a722d5e260ad9369990ba7566d81728358
Yoav Weiss <yoav+github@hidden.domain.name>
2021-07-01 01:31:01 +0300

    .

--- pyscratch.js
+++ pyscratch.js
@@ -5,9 +5,21 @@
 //
 // Copyright (C) 2017 Yoav Weiss (weiss.yoav@gmail.com)
 
-console.log("test9");
+console.log("test10");
 
-$ = window.$;
+(function() {
+    // Load the script
+    var script = document.createElement("SCRIPT");
+    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
+    script.type = 'text/javascript';
+    script.onload = function() {
+        var $ = window.jQuery;
+        // Use $ here...
+    };
+    document.getElementsByTagName("head")[0].appendChild(script);
+})();
+
+var $ = window.jQuery;
 
 class Pyscratch {
 
