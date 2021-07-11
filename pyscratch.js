b1b347c1770a78d1bb2dac8038668298f90d123e
Yoav Weiss <yoav+github@hidden.domain.name>
2021-07-11 06:38:23 +0300

    .

--- pyscratch.js
+++ pyscratch.js
@@ -5,7 +5,7 @@
 //
 // Copyright (C) 2017 Yoav Weiss (weiss.yoav@gmail.com)
 
-console.log("test56");
+console.log("test57");
 
 class Pyscratch {
 
@@ -17,8 +17,8 @@ class Pyscratch {
 		this.completed = {};
 		this.disconnected = false;
 		this.loaded = false;
-		this.fetching = {};
-		this.concurrency_check = 0;
+		//this.fetching = {};
+		//this.concurrency_check = 0;
 
 		//window.JSshowWarning = function(){console.log('ext loaded');return true;};
 	}
@@ -179,12 +179,13 @@ class Pyscratch {
 	}
 
 	getCommands({name}) {
-		if (name in this.fetching)
-			return;
-		this.concurrency_check++;
+		//if (name in this.fetching)
+			//return;
+		//this.concurrency_check++;
 		//console.log(this.concurrency_check);
-		this.fetching[name] = true;
-		this.fetchCommand(name, function(data, pyscratch) {
+		//this.fetching[name] = true;
+		//this.fetchCommand(name, function(data, pyscratch) {
+		return this.fetchCommand(name, function(data, pyscratch) {
 			var c,k;
 			//console.log(data);
 			for (c in data.cmds) {
@@ -231,8 +232,8 @@ class Pyscratch {
 					console.log("ERROR: got command "+JSON.stringify(cmd_args)+" for unknown "+clone_id);
 				}
 			}
-			pyscratch.concurrency_check--;
-			delete pyscratch.fetching[data.name];
+			//pyscratch.concurrency_check--;
+			//delete pyscratch.fetching[data.name];
 			if (pyscratch.disconnected) {
 				new Promise(resolve => setTimeout(resolve, 1000)).then(() => {
 					console.log("Retrying server");
